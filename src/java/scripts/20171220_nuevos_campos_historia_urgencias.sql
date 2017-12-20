442e451ba55ffcdb81d9adc4956dc0d8dd19e809
casc <casc@casc>
2017-12-20 19:53:58 -0400

    agregar nuevosacmposa urgencias

---
+++ src/java/scripts/20171220_nuevos_campos_historia_urgencias.sql
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  casc
+ * Created: 20/12/2017
+ */
+
+
+INSERT INTO public.hc_campos_reg(
+            id_tipo_reg, nombre, tabla, posicion, nombre_pdf)
+    VALUES (20, 'COBERTURA', NULL, 121, 'COBERTURA');
+INSERT INTO public.hc_campos_reg(
+            id_tipo_reg, nombre, tabla, posicion, nombre_pdf)
+    VALUES (20, 'TRIAGE', NULL, 122, 'TRIAGE');
+INSERT INTO public.hc_campos_reg(
+            id_tipo_reg, nombre, tabla, posicion, nombre_pdf)
+    VALUES (20, 'ORIGEN DE ATENCION', NULL, 123, 'ORIGEN DE ATENCION');
+INSERT INTO public.hc_campos_reg(
+            id_tipo_reg, nombre, tabla, posicion, nombre_pdf)
+    VALUES (20, 'REMITIDO', NULL, 124, 'REMITIDO');
+INSERT INTO public.hc_campos_reg(
+            id_tipo_reg, nombre, tabla, posicion, nombre_pdf)
+    VALUES (20, 'DESTINO', NULL, 125, 'DESTINO');
